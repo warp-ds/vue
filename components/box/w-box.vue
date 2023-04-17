f211d69acd557ca4ca7c91cad3be90adb76f0817
Magnus Hübner <magnus.hubner@aptitud.se>
2023-04-17 09:42:38 +0200

    fix: Updated w-box component with new warp classes, added todos for later and restructured the document

--- components/box/w-box.vue
+++ components/box/w-box.vue
@@ -1,3 +1,34 @@
+<script setup>
+import { box as ccBox } from '@warp-ds/component-classes';
+import { computed } from 'vue';
+
+const props = defineProps({
+  as: { type: String, default: 'div' },
+  bleed: Boolean,
+  // TODO: clickable should be marked as deprecated in the docs (and refer to use card instead) and then be removed.
+  clickable: Boolean,
+  // TODO: According to the Figma design info, neutral and bordered are variants and shouldn't be combinable.
+  //       It might be better to have a type/variant prop and have info or bordered as possible values?
+  //       ...and then treat neutral as the default when type is not set?
+  info: Boolean,
+  neutral: Boolean,
+  bordered: Boolean
+});
+
+const boxClasses = computed(() => [
+  ccBox.box,
+  {
+    [ccBox.bleed]: props.bleed,
+    'i-bg-$color-box-info-background': props.info,
+    'i-bg-$color-box-neutral-background': props.neutral,
+    'border-2 i-border-$color-box-bordered-border i-bg-$color-box-bordered-background': props.bordered,
+    'hover:active:i-bg-$color-box-info-background-hover': props.clickable && props.info,
+    'hover:active:i-bg-$color-box-neutral-background-hover': props.clickable && props.neutral,
+    'hover:i-bg-$color-box-bordered-background-hover active:i-bg-$color-box-bordered-background-hover hover:i-border-$color-box-bordered-border-hover active:i-border-$color-box-bordered-border-hover': props.clickable && props.bordered
+  }
+]);
+</script>
+
 <template>
   <component :is="as" :class="boxClasses">
     <slot />
@@ -5,28 +36,5 @@
 </template>
 
 <script>
-export default { name: 'wBox' }
-</script>
-
-<script setup>
-import { box as c } from '@fabric-ds/css/component-classes'
-import { computed } from 'vue'
-
-const props = defineProps({
-  bleed: Boolean,
-  as: { type: String, default: 'div' },
-  clickable: Boolean,
-  info: Boolean,
-  neutral: Boolean,
-  bordered: Boolean
-})
-const boxClasses = computed(() => ({
-  [c.box]: true,
-  [c.bleed]: props.bleed,
-  'bg-aqua-50': props.info,
-  'hover:bg-aqua-100 active:bg-aqua-200': props.info && props.clickable,
-  'bg-bluegray-50': props.neutral,
-  'hover:bg-bluegray-100 active:bg-bluegray-200': props.neutral && props.clickable,
-  'border-2 border-bluegray-300': props.bordered
-}))
+export default { name: 'wBox' };
 </script>
