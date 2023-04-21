0a44566ab9f02597be8f4c2cea7ad65beb550ace
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2023-04-21 14:48:05 +0200

    fix: add component classes to breadcrumbs (#19)

--- components/breadcrumbs/w-breadcrumbs.vue
+++ components/breadcrumbs/w-breadcrumbs.vue
@@ -1,7 +1,7 @@
 <template>
   <nav :aria-label="ariaLabel">
     <h2 class="sr-only">{{ ariaLabel }} </h2>
-    <div class="flex space-x-8">
+    <div :class="ccBreadcrumbs.wrapper">
       <breadcrumbify>
         <slot />
       </breadcrumbify>
@@ -9,27 +9,32 @@
   </nav>
 </template>
 
-<script>
+<script setup>
 import { h, Fragment } from 'vue'
-import { interleave } from '@fabric-ds/core/breadcrumbs'
+import { interleave } from '@warp-ds/core/breadcrumbs'
+import { breadcrumbs as ccBreadcrumbs } from "@warp-ds/component-classes"
 
-export const wBreadcrumbSeparator = h('span', { ariaHidden: true, class: 'select-none' }, '/')
+const props = defineProps({
+  ariaLabel: { type: String, default: 'Her er du' }
+});
+
 const isFragment = vnode => vnode.type === Fragment
 const collectElements = (vnodes = []) => vnodes?.map(vnode => isFragment(vnode) ? collectElements(vnode.children) : vnode)
 const Breadcrumbify = (_, context) => {
   const slot = context.slots.default()
   const vnodes = slot[0].children
   const elements = collectElements(vnodes).flat(Infinity)
-  return interleave(elements, wBreadcrumbSeparator)
-}
+  const styledElements = elements.map(el => {
+    const ccClasses = el.type === "a" ? ccBreadcrumbs.link : ccBreadcrumbs.text
+    el.props.class = el.props.class ? `${el.props.class} ${ccClasses}` : ccClasses
 
-// because most of the logic is in Breadcrumbify
-// there's no reason to script-setup this component
-export default {
-  name: 'wBreadcrumbs',
-  components: { Breadcrumbify },
-  props: {
-    ariaLabel: { type: String, default: 'Her er du' }
-  }
+    return el
+  })
+  return interleave(styledElements, wBreadcrumbSeparator)
 }
+</script>
+
+<script>
+  export const wBreadcrumbSeparator = h('span', { ariaHidden: true, class: ccBreadcrumbs.separator }, '/')
+  export default { name: 'wBreadcrumbs' }
 </script>
