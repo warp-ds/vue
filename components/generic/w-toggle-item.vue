4526148123a9d4b8240b2086ea757e066ad2dad9
AnnaRybkina <37986637+AnnaRybkina@users.noreply.github.com>
2023-05-09 13:12:03 +0200

    fix: add component classes to the toggle component (#24)

--- components/generic/w-toggle-item.vue
+++ components/generic/w-toggle-item.vue
@@ -1,23 +1,63 @@
 <template>
-  <input :id="id" v-model="model" v-bind="$attrs" />
-  <label v-if="label" :for="id" v-html="label" :class="labelClass" />
-  <label v-else :for="id" :class="labelClass"><slot /></label>
+  <input 
+    :id="id" 
+    v-model="model" 
+    :type="type"
+    :radioButton="radioButton"
+    :disabled="disabled"
+    :invalid="invalid"
+    :equalWidth="equalWidth"
+    v-bind="$attrs" 
+    :class="[inputClasses]" 
+  />
+  <label v-if="label" :for="id" v-html="label" :class="labelClasses" />
+  <label v-else :for="id" :class="labelClasses"><slot /></label>
 </template>
 
-<script>
-import { id } from '#util'
-import { modelProps, createModel } from 'create-v-model'
+<script setup>
+import { computed } from 'vue';
+import { id as uniqueId } from '#util';
+import { modelProps, createModel } from 'create-v-model';
+import { toggle as ccToggle } from '@warp-ds/component-classes';
+const p = defineProps({
+  id: { ...uniqueId },
+  label: String,
+  type: String,
+  class: String,
+  disabled: Boolean,
+  equalWidth: Boolean,
+  invalid: Boolean,
+  radioButton: Boolean,
+  labelClass: null,
+  ...modelProps()
+});
 
-export default {
-  name: 'wToggleItem',
-  props: {
-    id,
-    label: String,
-    labelClass: null,
-    ...modelProps()
-  },
-  setup: (props, { emit }) => ({
-    model: createModel({ props, emit })
-  })
-}
+const emit = defineEmits(['update:modelValue']);
+const model = createModel({ props: p, emit });
+
+const isRadio = computed(() => p.type === 'radio');
+const isCheckbox = computed(() => p.type === 'checkbox');
+const labelClasses = computed(() => (p.labelClass || {
+  [ccToggle.label]: !p.radioButton,
+  [`${ccToggle.labelDisabled} ${isCheckbox.value ? ccToggle.checkboxDisabled : ccToggle.radioDisabled}`] : p.disabled,
+  [ccToggle.focusable]: !p.radioButton,
+  [ccToggle.noContent]: !p.radioButton,
+  [ccToggle.labelRadioColors] : isRadio.value && !p.radioButton,
+  [ccToggle.radio]: isRadio.value,
+  [ccToggle.radioInvalid]: isRadio.value && p.invalid,
+  [ccToggle.checkboxInvalid]: isCheckbox.value && p.invalid,
+  [`${ccToggle.checkbox} ${ccToggle.labelCheckboxColors} ${ccToggle.icon} ${p.disabled ? '' : ccToggle.checkboxChecked}`]: isCheckbox.value,
+  [ccToggle.scLabel]: p.radioButton,
+  [ccToggle.scLabelJustified]: p.equalWidth,
+  [ccToggle.scLabelSmall]: p.small,
+}));
+const inputClasses = {
+  [p.class ? `${p.class} ${ccToggle.input}` : ccToggle.input]: true,
+};
+
+</script>
+
+<script>
+export default { name: 'wToggleItem' };
+
 </script>
