6f2b941577f9d1a6074b38fae2ab87106a21548a
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-22 13:43:36 +0200

    feat(attention): warpify attention

--- components/attention/w-attention.vue
+++ components/attention/w-attention.vue
@@ -1,6 +1,76 @@
+<script setup>
+  import { watch, computed, ref, onMounted, nextTick } from 'vue'
+  import { attention as ccAttention } from '@warp-ds/component-classes'
+  import { computePosition, flip, offset, shift, arrow } from '@floating-ui/dom'
+
+  import { absentProp } from '#util'
+  import { props as attentionProps, directions, computeCalloutArrow } from './attentionUtil.js'
+  import wAttentionArrow from './w-attention-arrow.vue'
+  import { createModel, modelProps } from 'create-v-model'
+
+  const props = defineProps({
+    ...attentionProps,
+    ...modelProps({ modelDefault: absentProp }),
+    targetEl: Object,
+    attentionClass: [Object, String],
+    attentionEl: {
+      default: () => ref(null)
+    },
+  })
+  
+  const emit = defineEmits(['update:modelValue']);
+  const directionName = computed(() => directions.find(e => props[e]));
+
+  const wrapperClasses = computed(() => ({
+    [ccAttention.base]: true,
+    [ccAttention.tooltip]: props.tooltip,
+    [ccAttention.callout]: props.callout,
+    [ccAttention.popover]: props.popover,
+    [ccAttention.arrowTooltip]: props.tooltip,
+    [ccAttention.arrowCallout]: props.callout,
+    [ccAttention.arrowPopover]: props.popover,
+  }))
+
+  const model = (props.modelValue === absentProp) ? ref(true) : createModel({ props, emit })
+  const arrowEl = ref(null)
+  const actualDirection = ref(directionName.value)
+
+  const recompute = async () => {
+    if (!model.value) return
+    await nextTick()
+    if(!props.attentionEl.value) return
+    if (props.callout) return computeCalloutArrow({ directionName, arrowEl, actualDirection })
+    
+    const position = await computePosition(props.targetEl, props.attentionEl.value, {
+      placement: directionName.value,
+      middleware: [
+        flip(),
+        offset(8),
+        shift({ padding: 16 }),
+        arrow({ element: props.noArrow ? undefined : arrowEl.value.$el })
+      ]
+    })
+
+    actualDirection.value = position.placement
+    Object.assign(props.attentionEl.value.style, {
+      left: '0',
+      top: '0',
+      transform: `translate3d(${Math.round(position.x)}px, ${Math.round(position.y)}px, 0)`
+    })
+    let { x, y } = position.middlewareData.arrow
+    arrowEl.value.$el.style.left = x ? (x + 'px') : null
+    arrowEl.value.$el.style.top = y ? (y + 'px') : null
+  }
+
+  onMounted(async () =>  {
+    watch(() => [props.top, props.bottom, props.left, props.right], recompute)
+    watch(model, recompute, { immediate: props.callout })
+  })
+</script>
+
 <template>
   <div :class="[{ 'absolute z-50': !callout }, attentionClass]" ref="attentionRef" v-show="model">
-    <div :class="wrapperClass" data-test="wrapper">
+    <div :class="wrapperClasses" data-test="wrapper">
       <w-attention-arrow v-bind="$props" v-if="!noArrow" ref="arrowEl" :direction="actualDirection" />
       <div class="last-child:mb-0"><slot /></div>
     </div>
@@ -8,67 +78,5 @@
 </template>
 
 <script>
-import { watch, computed, ref, onMounted, nextTick } from 'vue'
-import { absentProp } from '#util'
-import { props as attentionProps, directions, computeCalloutArrow } from './attentionUtil.js'
-import wAttentionArrow from './w-attention-arrow.vue'
-import { computePosition, flip, offset, shift, arrow } from '@floating-ui/dom'
-import { createModel, modelProps } from 'create-v-model'
-
-export default {
-  name: 'wAttention',
-  props: {
-    ...attentionProps,
-    ...modelProps({ modelDefault: absentProp }),
-    targetEl: Object,
-    attentionClass: [Object, String],
-    attentionEl: {
-      default: () => ref(null)
-    }
-  },
-  components: { wAttentionArrow },
-  setup: (props, { emit }) => {
-    const directionName = computed(() => directions.find(e => props[e]))
-    const wrapperClass = computed(() => ({
-      ['border-2 relative']: true,
-      ['bg-gray-700 border-gray-700 text-white rounded-4 py-6 px-8']: props.tooltip,
-      ['bg-green-100 border-green-400 py-8 px-16 rounded-8']: props.callout,
-      ['bg-white border-white rounded-8 p-16 filter drop-shadow-20']: props.popover,
-    }))
-
-    const model = (props.modelValue === absentProp) ? ref(true) : createModel({ props, emit })
-    const arrowEl = ref(null)
-    const actualDirection = ref(directionName.value)
-    const recompute = async () => {
-      if (!model.value) return
-      await nextTick()
-      if (props.callout) return computeCalloutArrow({ directionName, arrowEl, actualDirection })
-      const position = await computePosition(props.targetEl, props.attentionEl.value, {
-        placement: directionName.value,
-        middleware: [
-          flip(),
-          offset(8),
-          shift({ padding: 16 }),
-          arrow({ element: props.noArrow ? undefined : arrowEl.value.$el })
-        ]
-      })
-      actualDirection.value = position.placement
-      Object.assign(props.attentionEl.value.style, {
-        left: '0',
-        top: '0',
-        transform: `translate3d(${Math.round(position.x)}px, ${Math.round(position.y)}px, 0)`
-      })
-      let { x, y } = position.middlewareData.arrow
-      arrowEl.value.$el.style.left = x ? (x + 'px') : null
-      arrowEl.value.$el.style.top = y ? (y + 'px') : null
-    }
-
-    onMounted(async () =>  {
-      watch(() => [props.top, props.bottom, props.left, props.right], recompute)
-      watch(model, recompute, { immediate: props.callout })
-    })
-
-    return { wrapperClass, attentionRef: props.attentionEl, arrowEl, actualDirection, model }
-  }
-}
+export default { name: 'wAttention' }
 </script>
