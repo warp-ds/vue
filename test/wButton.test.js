2a832fc80ee44559ae09feb2d64d9a3ded69483e
CarolineGard <caroline.gard@mpyadigital.com>
2023-05-25 14:21:44 +0200

    fix(button): fix test

--- test/wButton.test.js
+++ test/wButton.test.js
@@ -1,5 +1,6 @@
 import { describe, test, assert } from 'vitest'
 import { mount } from '@vue/test-utils'
+import { button as ccButton } from '@warp-ds/component-classes';
 import { wButton } from '#components'
 
 const label =  'Hello Warp'
@@ -20,7 +21,7 @@ describe('button', () => {
   test('primary', () => {
     const wrapper = mount(wButton, { props: { primary: true, label } })
     assert.equal(wrapper.text(), 'Hello Warp')
-    assert.include(wrapper.classes().join(' '), 'py-12 px-16 border-0 font-bold rounded-8 leading-24 max-w-max focusable justify-center transition-colors ease-in-out i-text-$color-button-primary-text hover:i-text-$color-button-primary-text i-bg-$color-button-primary-background hover:i-bg-$color-button-primary-background-hover! active:i-bg-$color-button-primary-background-active')
+    assert.include(wrapper.classes().join(' '), ccButton.buttonPrimary)
   })
   test('href', () => {
     const href = 'https://finn.no'
