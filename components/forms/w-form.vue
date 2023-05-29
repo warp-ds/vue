f7125288b7d87866fee41e4c73ca44841210065d
Caroline Gard <caroline.gard@mpyadigital.com>
2023-05-29 16:11:25 +0200

    feat(Form): warpify forms (#36)
    * fix(form): refactor form component
    
    * fix(form): change component name

--- components/forms/w-form.vue
+++ components/forms/w-form.vue
@@ -1,18 +1,10 @@
-<template>
-  <component :is="as">
-    <slot />
-  </component>
-</template>
+<script setup>
+  import { watchEffect } from 'vue'
+  import { absentProp } from '#util'
+  import { modelProps } from 'create-v-model'
+  import { createValidationCollector } from './validation'
 
-<script>
-import { watchEffect } from 'vue'
-import { absentProp } from '#util'
-import { modelProps } from 'create-v-model'
-import { createValidationCollector } from './validation'
-
-export default {
-  name: 'wForm',
-  props: {
+  const props = defineProps({
     as: {
       default: 'form'
     },
@@ -22,12 +14,23 @@ export default {
     },
     ...modelProps({ modelDefault: absentProp }),
     ...modelProps({ modelName: 'completed', modelDefault: absentProp }),
-  },
-  setup(props, { emit }) {
-    const { allChildrenValid, completed, childrenShouldValidate } = createValidationCollector()
-    if (props.modelValue !== absentProp) watchEffect(() => emit('update:modelValue', allChildrenValid.value))
-    if (props.completed !== absentProp) watchEffect(() => emit('update:completed', completed.value))
-    if (props.shouldValidate !== absentProp) watchEffect(() => childrenShouldValidate.value = props.shouldValidate)
-  }
-}
+  })
+
+  const emit = defineEmits(['update:modelValue']);
+  const { allChildrenValid, completed, childrenShouldValidate } = createValidationCollector()
+
+  if (props.modelValue !== absentProp) watchEffect(() => emit('update:modelValue', allChildrenValid.value))
+  if (props.completed !== absentProp) watchEffect(() => emit('update:completed', completed.value))
+  if (props.shouldValidate !== absentProp) watchEffect(() => childrenShouldValidate.value = props.shouldValidate)
 </script>
+
+<template>
+  <component :is="as">
+    <slot />
+  </component>
+</template>
+
+<script>
+export default { name: 'wForm' };
+</script>
+
