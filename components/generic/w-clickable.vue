9d8e92d6c95f8cb0e9664e8e302a112d5cd5d78f
Anna Rybkina <anna.rybkina@anicode.se>
2023-05-30 16:41:37 +0200

    fix: focusable is added to the component classes

--- components/generic/w-clickable.vue
+++ components/generic/w-clickable.vue
@@ -11,24 +11,18 @@ const props = defineProps({
   checkbox: Boolean
 });
 const type = computed(() => props.radio ? 'radio' : 'checkbox');
-
-const clickableClasses = computed(() => ({
-  ['focus:focusable:focus focus-visible:focusable:focus-visible not-focus-visible:focusable:focus:not(:focus-visible) focusable-inset']: true,
-  [ccClickable.clickable]: true,
-}));
 const labelClasses = computed(() => ({
-  ['focus:focusable:focus focus-visible:focusable:focus-visible not-focus-visible:focusable:focus:not(:focus-visible) focusable-inset']: true,
   [ccClickable.label]: props.label,
 }));
 
 </script>
 
 <template>
-  <w-toggle-item v-if="radio || checkbox" :class="clickableClasses" :type="type" :label-class="labelClasses" v-bind="$attrs">
+  <w-toggle-item v-if="radio || checkbox" :class="ccClickable.clickable" :type="type" :label-class="labelClasses" v-bind="$attrs">
     <slot />
   </w-toggle-item>
   <component v-else :is="href ? 'a' : 'button'" :class="labelClasses" :href="href" :type="href ? undefined : ($attrs.type || 'button')">
-    <span :class="clickableClasses" aria-hidden="true" />
+    <span :class="ccClickable.clickable" aria-hidden="true" />
     <slot />
   </component>
 </template>
