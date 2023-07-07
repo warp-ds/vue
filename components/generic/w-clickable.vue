89342222afa7e5634cc3b113793643bb79e42ee8
BalbinaK <balbuhhha@gmail.com>
2023-07-07 12:17:21 +0200

    fix(clickable): use imported classes

--- components/generic/w-clickable.vue
+++ components/generic/w-clickable.vue
@@ -14,15 +14,19 @@ const type = computed(() => props.radio ? 'radio' : 'checkbox');
 const labelClasses = computed(() => ({
   [ccClickable.label]: props.label,
 }));
+const buttonOrLinkClasses = computed(() => ({
+  [ccClickable.buttonOrLink]: true,
+  [ccClickable.label]: props.label,
+}))
 
 </script>
 
 <template>
-  <w-toggle-item v-if="radio || checkbox" :class="ccClickable.clickable" :type="type" :label-class="labelClasses" v-bind="$attrs">
+  <w-toggle-item v-if="radio || checkbox" :class="ccClickable.toggle" :type="type" :label-class="labelClasses" v-bind="$attrs">
     <slot />
   </w-toggle-item>
-  <component v-else :is="href ? 'a' : 'button'" :class="labelClasses" :href="href" :type="href ? undefined : ($attrs.type || 'button')">
-    <span :class="ccClickable.clickable" aria-hidden="true" />
+  <component v-else :is="href ? 'a' : 'button'" :class=buttonOrLinkClasses :href="href" :type="href ? undefined : ($attrs.type || 'button')">
+    <span :class=[ccClickable.buttonOrLinkStretch] aria-hidden="true" />
     <slot />
   </component>
 </template>
