9242b64efed5935e78aeb927a2f1efdf9c967bc7
BalbinaK <balbuhhha@gmail.com>
2023-07-12 16:25:43 +0200

    fix: use resets from Eik at 1.0.0-alpha.33

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import { presetWarp } from '@warp-ds/uno'
 import uno from 'unocss/vite'
 import { MinifyWarpLib } from './.minifier-plugin.js'
 import VitEik from 'viteik'
-import { classes } from '@warp-ds/component-classes/classes';
+import { classes } from '@warp-ds/css/component-classes/classes';
 
 export default defineConfig((env) => ({
   plugins: [
     vue(),
     uno({
-      presets: [presetWarp({ development: true })],
+      presets: [presetWarp({ skipResets: true })],
       safelist: classes,
     }),
     env.mode !== 'lib' && VitEik(),
