4b32bbb6542065ad3b30048d60aee03ff91b8288
Silje Christensen <silje.christensen@cognite.com>
2023-08-03 19:29:49 +0200

    fix: add default extractor (handles .js files) + add translation for validation

--- lingui.config.ts
+++ lingui.config.ts
@@ -1,6 +1,7 @@
 import type { LinguiConfig } from '@lingui/conf';
 import { vueExtractor } from '@lingui/extractor-vue';
-
+// @ts-expect-error issue in typings
+import { extractor as defaultExtractor } from '@lingui/cli/api';
 const config: LinguiConfig = {
   locales: ['en', 'nb'],
   catalogs: [
@@ -27,7 +28,7 @@ const config: LinguiConfig = {
   ],
   sourceLocale: 'en',
   compileNamespace: 'es',
-  extractors: [vueExtractor],
+  extractors: [defaultExtractor, vueExtractor],
 };
 
 export default config;
