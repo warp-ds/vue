c7bed6df0885d4994efd747635ca36f31cca3f6e
Anna Rybkina <anna.rybkina@anicode.se>
2023-09-19 11:12:49 +0200

    fix: remove useless test and update changelog

--- test/wButton.test.js
+++ test/wButton.test.js
@@ -40,10 +40,6 @@ describe('button', () => {
     const wrapper = mount(wButton, { props: { small: true, quiet: true } })
     assert.include(wrapper.classes().join(' '), ccButton.secondarySmallQuiet)
   })
-  test('small & quiet & loading with no variant should default to secondary small quiet loading', () => {
-    const wrapper = mount(wButton, { props: { small: true, quiet: true, loading: true } })
-    assert.include(wrapper.classes().join(' '), ccButton.secondarySmallQuietLoading)
-  })
   test('href', () => {
     const href = 'https://finn.no'
     const wrapper = mount(wButton, { props: { label, href } })
