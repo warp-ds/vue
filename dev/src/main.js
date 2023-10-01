63e5e33eff38330f09a944d7cb9c247ed6a7fbd2
Dave Honneffer <pearofducks@gmail.com>
2023-10-01 13:27:39 +0200

    chore: substitute shikiji for shiki

--- dev/src/main.js
+++ dev/src/main.js
@@ -2,21 +2,28 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import Cleave from 'cleave-lite';
 import { router } from './router'
-import { setWasm, setCDN, getHighlighter } from 'shiki'
+import { getHighlighterCore } from 'shikiji/core'
+import { getWasmInlined } from 'shikiji/wasm'
+import vitesseLight from 'shikiji/themes/vitesse-light.mjs'
 import Control from './Control.vue'
 import Controls from './Controls.vue'
 import ComponentTitle from './ComponentTitle.vue'
 import { Token } from '@itsy/token'
 import 'uno.css'
 
-setWasm('/shiki/dist/onig.wasm')
-setCDN('/shiki/')
-
-const highlighter = await getHighlighter({ theme: 'vitesse-light', langs: ['js', 'vue-html'] })
+const highlighter = await getHighlighterCore({
+  themes: [vitesseLight],
+  langs: [
+    import('shikiji/langs/javascript.mjs'),
+    import('shikiji/langs/vue-html.mjs')
+  ],
+  loadWasm: getWasmInlined
+})
 
 createApp(App)
   .provide('Cleave', Cleave)
   .provide('highlighter', highlighter)
+  .provide('highlightTheme', 'vitesse-light')
   .component('token', Token)
   .component('demo-control', Control)
   .component('demo-controls', Controls)
