360bc365b8cb3b2c30c612d2cd32353b41f464dd
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2023-10-16 16:18:11 +0200

    adjust formatting of functions

--- components/attention/w-attention.vue
+++ components/attention/w-attention.vue
@@ -15,23 +15,24 @@
     targetEl: Object,
     attentionClass: [Object, String],
     attentionEl: {
-      default: () => ref(null),
+      default: () => ref(null)
     },
   })
+
 
   const emit = defineEmits(['update:modelValue'])
   const directionName = computed(() => directions.find((e) => props[e]))
 
   const attentionClasses = computed(() => ({
     [props.attentionClass]: true,
-    [ccAttention.notCallout]: !props.callout,
+    [ccAttention.notCallout]: !props.callout
   }))
 
   const wrapperClasses = computed(() => ({
     [ccAttention.base]: true,
     [ccAttention.tooltip]: props.tooltip,
     [ccAttention.callout]: props.callout,
-    [ccAttention.popover]: props.popover,
+    [ccAttention.popover]: props.popover
   }))
 
   const model =
@@ -42,35 +43,28 @@
   const recompute = async () => {
     if (!model.value) return
     await nextTick()
-    if (props.callout)
-      return computeCalloutArrow({ directionName, arrowEl, actualDirection })
+    if (props.callout) return computeCalloutArrow({ directionName, arrowEl, actualDirection })
     if (!props.attentionEl.value) return
 
-    const position = await computePosition(
-      props.targetEl,
-      props.attentionEl.value,
-      {
-        placement: directionName.value,
-        middleware: [
-          flip(),
-          offset(8),
-          shift({ padding: 16 }),
-          arrow({ element: props.noArrow ? undefined : arrowEl.value.$el }),
-        ],
-      }
-    )
+    const position = await computePosition(props.targetEl, props.attentionEl.value, {
+      placement: directionName.value,
+      middleware: [
+        flip(),
+        offset(8),
+        shift({ padding: 16 }),
+        arrow({ element: props.noArrow ? undefined : arrowEl.value.$el })
+      ]
+    })
 
     actualDirection.value = position.placement
     Object.assign(props.attentionEl.value.style, {
       left: '0',
       top: '0',
-      transform: `translate3d(${Math.round(position.x)}px, ${Math.round(
-        position.y
-      )}px, 0)`,
+      transform: `translate3d(${Math.round(position.x)}px, ${Math.round(position.y)}px, 0)`,
     })
     let { x, y } = position.middlewareData.arrow
-    arrowEl.value.$el.style.left = x ? x + 'px' : null
-    arrowEl.value.$el.style.top = y ? y + 'px' : null
+    arrowEl.value.$el.style.left = x ? (x + 'px') : null
+    arrowEl.value.$el.style.top = y ? (y + 'px') : null
   }
 
   const activeAttentionProp = computed(() =>
