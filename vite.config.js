5937be68a93c02857f0e857a8cc1b982af0f2094
Anna Rybkina <anna.rybkina@anicode.se>
2023-10-17 15:44:20 +0200

    chore: add workflow to publish docs

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,50 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { presetWarp } from '@warp-ds/uno'
-import uno from 'unocss/vite'
-import { MinifyWarpLib } from './.minifier-plugin.js'
-import VitEik from 'viteik'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import { presetWarp } from '@warp-ds/uno';
+import uno from 'unocss/vite';
+import { MinifyWarpLib } from './.minifier-plugin.js';
+import VitEik from 'viteik';
 import { classes } from '@warp-ds/css/component-classes/classes';
 
-export default defineConfig((env) => ({
+const asyncPreset = await presetWarp({
+  skipResets: true,
+  omitComponentClasses: true,
+});
+
+export default defineConfig(async (env) => ({
   plugins: [
     vue(),
     uno({
-      presets: [presetWarp({ skipResets: true })],
+      presets: [asyncPreset],
       safelist: classes,
     }),
     env.mode !== 'lib' && VitEik(),
     MinifyWarpLib(),
   ],
-  server: { host: "0.0.0.0", port: 3003 },
+  server: { host: '0.0.0.0', port: 3003 },
   test: { environment: 'happy-dom' },
-  ...getBuildOpts(env)
-}))
+  ...getBuildOpts(env),
+}));
 
 function getBuildOpts(env) {
-  if (env.mode === 'production') return defineConfig({
-    build: { target: 'esnext' }
-  })
-  if (env.mode === 'lib') return defineConfig({
-    build: {
-      ...getLibOpts('warp-vue'),
-      rollupOptions: { external: ['vue'] }
-    }
-  })
-  if (env.mode === 'eik') return defineConfig({
-    build: {
-      emptyOutDir: false,
-      ...getLibOpts('warp-vue.eik')
-    }
-  })
+  if (env.mode === 'production')
+    return defineConfig({
+      build: { target: 'esnext' },
+    });
+  if (env.mode === 'lib')
+    return defineConfig({
+      build: {
+        ...getLibOpts('warp-vue'),
+        rollupOptions: { external: ['vue'] },
+      },
+    });
+  if (env.mode === 'eik')
+    return defineConfig({
+      build: {
+        emptyOutDir: false,
+        ...getLibOpts('warp-vue.eik'),
+      },
+    });
 }
 
 function getLibOpts(fileName) {
@@ -45,7 +53,7 @@ function getLibOpts(fileName) {
     lib: {
       formats: ['es'],
       entry: './index.js',
-      fileName
+      fileName,
     },
-  }
+  };
 }
