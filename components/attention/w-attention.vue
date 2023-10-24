37b6165ad1e9b7dfc71f75772fdaff2de84b5409
felicia-haggqvist <144792260+felicia-haggqvist@users.noreply.github.com>
2023-10-24 13:36:04 +0200

    fix: a11y issues attention (#85)
    * WIP fix a11y issues for attention component
    
    * remove redundant aria-props
    
    * Fix a11y issues for tooltip attention
    
    * fix a11y issues for callout + popover attention
    
    * revert formatting of w-attention
    
    * add dynamic role + aria-label attributes in w-attention
    
    * remove redundant console.log
    
    * remove aria-label for w-attention-arrow
    
    * replace h4-class with h4-tag
    
    * remove redundant arrowLabels from attentionUtil
    
    * refactor pointingAt computed function
    
    * change to ternary in pointingAt computed function
    
    * refactor activeAttentionProp-computed function to use ternary expression instead
    
    * format to use single quotes
    
    * format to remove semi-colon
    
    * fix indentation
    
    * adjust formatting of functions
    
    * remove redundant space
    
    * adjust format of model
    
    * adjust format
    
    * adjust format
    
    * handle translations for w-attention
    
    * rename translatedDirection to pointingAtDirection + add story for resetting role and aria-label
    
    * set format indentation to 2 spaces
    
    * rename direction id for i18n
    
    * use indentation with spaces instead of tab
    
    * move role + aria-label to wrapper-div + replace aria-describedby with aria-details
    
    * remove 'speech bubble' from tooltip message
    
    * add role + ariaLabel as props to allow user to reset/override default
    
    * return props.ariaLabel if user has inserted a string
    
    * change from null to undefined

--- components/attention/w-attention.vue
+++ components/attention/w-attention.vue
@@ -1,79 +1,185 @@
 <script setup>
-  import { watch, computed, ref, onMounted, nextTick } from 'vue'
-  import { attention as ccAttention } from '@warp-ds/css/component-classes'
-  import { computePosition, flip, offset, shift, arrow } from '@floating-ui/dom'
+import { watch, computed, ref, onMounted, nextTick } from 'vue'
+import { attention as ccAttention } from '@warp-ds/css/component-classes'
+import { computePosition, flip, offset, shift, arrow } from '@floating-ui/dom'
 
-  import { absentProp } from '#util'
-  import { props as attentionProps, directions, computeCalloutArrow } from './attentionUtil.js'
-  import wAttentionArrow from './w-attention-arrow.vue'
-  import { createModel, modelProps } from 'create-v-model'
+import { absentProp } from '#util'
+import {
+  props as attentionProps,
+  directions,
+  computeCalloutArrow,
+} from './attentionUtil.js'
+import { opposites } from '@warp-ds/core/attention'
+import wAttentionArrow from './w-attention-arrow.vue'
+import { createModel, modelProps } from 'create-v-model'
+import { i18n } from '@lingui/core'
+import { activateI18n } from '../util/i18n'
+import { messages as enMessages } from './locales/en/messages.mjs'
+import { messages as nbMessages } from './locales/nb/messages.mjs'
+import { messages as fiMessages } from './locales/fi/messages.mjs'
 
-  const props = defineProps({
-    ...attentionProps,
-    ...modelProps({ modelDefault: absentProp }),
-    targetEl: Object,
-    attentionClass: [Object, String],
-    attentionEl: {
-      default: () => ref(null)
-    },
-  })
-  
-  const emit = defineEmits(['update:modelValue']);
-  const directionName = computed(() => directions.find(e => props[e]));
+activateI18n(enMessages, nbMessages, fiMessages)
 
-  const attentionClasses = computed(() => ({
-    [props.attentionClass]: true,
-    [ccAttention.notCallout]: !props.callout
-  }))
-  
-  const wrapperClasses = computed(() => ({
-    [ccAttention.base]: true,
-    [ccAttention.tooltip]: props.tooltip,
-    [ccAttention.callout]: props.callout,
-    [ccAttention.popover]: props.popover
-  }))
+const props = defineProps({
+  ...attentionProps,
+  ...modelProps({ modelDefault: absentProp }),
+  targetEl: Object,
+  attentionClass: [Object, String],
+  attentionEl: {
+    default: () => ref(null),
+  },
+  role: String,
+  ariaLabel: String,
+})
 
-  const model = (props.modelValue === absentProp) ? ref(true) : createModel({ props, emit })
-  const arrowEl = ref(null)
-  const actualDirection = ref(directionName.value)
+const emit = defineEmits(['update:modelValue'])
+const directionName = computed(() => directions.find((e) => props[e]))
 
-  const recompute = async () => {
-    if (!model.value) return
-    await nextTick()
-    if (props.callout) return computeCalloutArrow({ directionName, arrowEl, actualDirection })
-    if(!props.attentionEl.value) return
+const attentionClasses = computed(() => ({
+  [props.attentionClass]: true,
+  [ccAttention.notCallout]: !props.callout,
+}))
 
-    const position = await computePosition(props.targetEl, props.attentionEl.value, {
+const wrapperClasses = computed(() => ({
+  [ccAttention.base]: true,
+  [ccAttention.tooltip]: props.tooltip,
+  [ccAttention.callout]: props.callout,
+  [ccAttention.popover]: props.popover,
+}))
+
+const model =
+  props.modelValue === absentProp ? ref(true) : createModel({ props, emit })
+const arrowEl = ref(null)
+const actualDirection = ref(directionName.value)
+
+const recompute = async () => {
+  if (!model.value) return
+  await nextTick()
+  if (props.callout)
+    return computeCalloutArrow({ directionName, arrowEl, actualDirection })
+  if (!props.attentionEl.value) return
+
+  const position = await computePosition(
+    props.targetEl,
+    props.attentionEl.value,
+    {
       placement: directionName.value,
       middleware: [
         flip(),
         offset(8),
         shift({ padding: 16 }),
-        arrow({ element: props.noArrow ? undefined : arrowEl.value.$el })
-      ]
-    })
+        arrow({ element: props.noArrow ? undefined : arrowEl.value.$el }),
+      ],
+    }
+  )
 
-    actualDirection.value = position.placement
-    Object.assign(props.attentionEl.value.style, {
-      left: '0',
-      top: '0',
-      transform: `translate3d(${Math.round(position.x)}px, ${Math.round(position.y)}px, 0)`
-    })
-    let { x, y } = position.middlewareData.arrow
-    arrowEl.value.$el.style.left = x ? (x + 'px') : null
-    arrowEl.value.$el.style.top = y ? (y + 'px') : null
-  }
-
-  onMounted(async () =>  {
-    watch(() => [props.top, props.bottom, props.left, props.right], recompute)
-    watch(model, recompute, { immediate: props.callout })
+  actualDirection.value = position.placement
+  Object.assign(props.attentionEl.value.style, {
+    left: '0',
+    top: '0',
+    transform: `translate3d(${Math.round(position.x)}px, ${Math.round(
+      position.y
+    )}px, 0)`,
   })
+  let { x, y } = position.middlewareData.arrow
+  arrowEl.value.$el.style.left = x ? x + 'px' : null
+  arrowEl.value.$el.style.top = y ? y + 'px' : null
+}
+
+// TODO: See if we can move this function to the core repo:
+const pointingAtDirection = computed(() => {
+  switch (opposites[actualDirection.value]) {
+    case 'top':
+      return i18n._({
+        id: 'attention.pointingUp',
+        message: 'pointing up',
+        comment:
+          'Default screenreader message for top direction in the attention component',
+      })
+    case 'right':
+      return i18n._({
+        id: 'attention.pointingRight',
+        message: 'pointing right',
+        comment:
+          'Default screenreader message for right direction in the attention component',
+      })
+    case 'bottom':
+      return i18n._({
+        id: 'attention.pointingDown',
+        message: 'pointing down',
+        comment:
+          'Default screenreader message for bottom direction in the attention component',
+      })
+    case 'left':
+      return i18n._({
+        id: 'attention.pointingLeft',
+        message: 'pointing left',
+        comment:
+          'Default screenreader message for left direction in the attention component',
+      })
+    default:
+      return ''
+  }
+})
+
+// TODO: See if we can move this function to the core repo:
+const activeAttentionType = computed(() => {
+  switch (true) {
+    case props.tooltip:
+      return i18n._({
+        id: 'attention.tooltip',
+        message: 'tooltip',
+        comment:
+          'Default screenreader message for tooltip in the attention component',
+      })
+    case props.callout:
+      return i18n._({
+        id: 'attention.callout',
+        message: 'callout speech bubble',
+        comment:
+          'Default screenreader message for callout speech bubble in the attention component',
+      })
+    case props.popover:
+      return i18n._({
+        id: 'attention.popover',
+        message: 'popover speech bubble',
+        comment:
+          'Default screenreader message for popover speech bubble in the attention component',
+      })
+    default:
+      return ''
+  }
+})
+
+// TODO: See if we can move this function to the core repo:
+const defaultAriaLabel = computed(() => {
+  return `${activeAttentionType.value} ${
+    !props.noArrow ? pointingAtDirection.value : ''
+  }`
+})
+
+onMounted(async () => {
+  watch(() => [props.top, props.bottom, props.left, props.right], recompute)
+  watch(model, recompute, { immediate: props.callout })
+})
 </script>
 
 <template>
-  <div :class="attentionClasses" ref="attentionRef" v-show="model">
-    <div :class="wrapperClasses" data-test="wrapper">
-      <w-attention-arrow v-bind="$props" v-if="!noArrow" ref="arrowEl" :direction="actualDirection" />
+  <div tabindex="0" :class="attentionClasses" ref="attentionRef" v-show="model">
+    <div
+      :role="props.role === '' ? undefined : props.tooltip ? 'tooltip' : 'img'"
+      :aria-label="
+        props.ariaLabel === '' ? undefined : props.ariaLabel ?? defaultAriaLabel
+      "
+      :class="wrapperClasses"
+      data-test="wrapper"
+    >
+      <w-attention-arrow
+        v-bind="$props"
+        v-if="!noArrow"
+        ref="arrowEl"
+        :direction="actualDirection"
+      />
       <div :class="ccAttention.content"><slot /></div>
     </div>
   </div>
