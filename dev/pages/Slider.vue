d1d857be4c4a58452adca13548ba6437382c1814
Magnus Hübner <magnus.hubner@aptitud.se>
2023-11-08 15:04:53 +0100

    docs: Add aria-valuetext to Slider examples

--- dev/pages/Slider.vue
+++ dev/pages/Slider.vue
@@ -16,7 +16,7 @@ const slowNumber = ref(30);
     <token :state="smallNumber">
       <div class="flex gap-16">
         <w-textfield type="number" min="1" max="10" step="1" inputmode="numeric" v-model.number="smallNumber" />
-        <w-slider v-model="smallNumber" :min="1" :max="10" :step="1" label="Small numbers slider" />
+        <w-slider v-model="smallNumber" :aria-valuetext="smallNumber" :min="1" :max="10" :step="1" label="Small numbers slider" />
       </div>
     </token>
 
@@ -24,7 +24,7 @@ const slowNumber = ref(30);
     <token :state="largeNumber">
       <div class="flex gap-16">
         <w-textfield type="number" min="0" max="100000" step="10" inputmode="numeric" v-model.number="largeNumber" />
-        <w-slider v-model="largeNumber" :min="0" :max="10000" :step="10" label="Large numbers slider (with step of 10)" />
+        <w-slider v-model="largeNumber" :aria-valuetext="largeNumber" :min="0" :max="10000" :step="10" label="Large numbers slider (with step of 10)" />
       </div>
     </token>
 
@@ -32,7 +32,7 @@ const slowNumber = ref(30);
     <token :state="extremeNumber">
       <div class="flex gap-16">
         <w-textfield type="number" min="0" max="100000000" step="100" inputmode="numeric" v-model.number="extremeNumber" />
-        <w-slider v-model="extremeNumber" :min="0" :max="100000000" :step="100" label="Extreme numbers slider (with step of 100)" />
+        <w-slider v-model="extremeNumber" :aria-valuetext="extremeNumber" :min="0" :max="100000000" :step="100" label="Extreme numbers slider (with step of 100)" />
       </div>
     </token>
 
@@ -40,7 +40,7 @@ const slowNumber = ref(30);
     <token :state="slowNumber">
       <div class="flex gap-16">
         <w-textfield type="number" min="0" max="1000" step="1" inputmode="numeric" v-model.number="slowNumber" />
-        <w-slider v-model="slowNumber" :min="0" :max="1000" :step="1" prevent-acceleration label="Slider without keydown acceleration" />
+        <w-slider v-model="slowNumber" :aria-valuetext="slowNumber" :min="0" :max="1000" :step="1" prevent-acceleration label="Slider without keydown acceleration" />
       </div>
     </token>
   </div>
