7e82490be0826a8f4e9a1a46a5a75f652096be74
imprashast <imprashast@gmail.com>
2024-01-24 13:45:02 +0100

    Add a test case

--- components/alert/stories/Alert.stories.js
+++ components/alert/stories/Alert.stories.js
@@ -1,6 +1,8 @@
 import wAlert from '../w-alert.vue';
 import { ref } from 'vue';
 
+import { within, expect } from '@storybook/test';
+
 const showAlert = ref(true);
 
 export default { title: 'FeedbackIndicators/Alert', component: wAlert };
@@ -8,8 +10,15 @@ export default { title: 'FeedbackIndicators/Alert', component: wAlert };
 const Template = (args) => ({
     components: { wAlert },
     setup() { return { args, showAlert }; },
-    template: '<w-alert :model-value="showAlert" v-bind="args"><p>I am an excellent message for the user.</p></w-alert>',
+    template: '<w-alert v-model="showAlert" v-bind="args"><p>I am an excellent message for the user.</p></w-alert>',
 });
 
 export const MainStory = Template.bind({});
 MainStory.args = { info: true, title: "I am a title" };
+
+export const NegativeAlertTask = Template.bind({});
+NegativeAlertTask.args = { negative: true, title: 'This is "negative" variant of the alert component' };
+NegativeAlertTask.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  await expect(canvas.getByRole('alert')).toBeInTheDocument();
+};
