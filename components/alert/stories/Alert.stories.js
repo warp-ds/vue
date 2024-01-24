a95e206fb484bc0d70a4e7f3d6414cea4c29997d
imprashast <imprashast@gmail.com>
2024-01-24 13:33:49 +0100

    fix alert

--- components/alert/stories/Alert.stories.js
+++ components/alert/stories/Alert.stories.js
@@ -1,12 +1,15 @@
 import wAlert from '../w-alert.vue';
+import { ref } from 'vue';
+
+const showAlert = ref(true);
 
 export default { title: 'FeedbackIndicators/Alert', component: wAlert };
 
 const Template = (args) => ({
     components: { wAlert },
-    setup() { return { args }; },
-    template: '<w-alert v-bind="args"><p>I am an excellent message for the user.</p></w-alert>',
+    setup() { return { args, showAlert }; },
+    template: '<w-alert :model-value="showAlert" v-bind="args"><p>I am an excellent message for the user.</p></w-alert>',
 });
 
 export const MainStory = Template.bind({});
-MainStory.args = { info: true, title: "I am a title", 'v-model': true };
+MainStory.args = { info: true, title: "I am a title" };
