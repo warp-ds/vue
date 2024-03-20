fa3a621b8ade3278bcca593e1e72639670c3a02e
Balbina K <41303231+BalbinaK@users.noreply.github.com>
2024-03-20 16:42:08 +0100

    fix(w-field): use correct colors in label and hint/error text (#150)
    * fix(field): remove invalid label color
    
    Based on the design, the label should always have the same color, even if input has invalid state
    
    * fix(field): fix hint/helpText color in invalid and regular state
    
    * chore(deps): update @warp-ds/css to 1.9.1

--- components/forms/w-field.vue
+++ components/forms/w-field.vue
@@ -1,9 +1,9 @@
 <template>
   <component :is="as" :class="{[ccInput.wrapper]: true, [$attrs.class || '']: true}" :role="role" v-bind="wrapperAria">
-    <component :is="labelType" v-if="label" :class="{[ccLabel.label]: true, [ccLabel.labelInvalid]: isInvalid}" :id="labelId" :for="labelFor" :role="valueOrUndefined(labelLevel, 'heading')" :aria-level="valueOrUndefined(labelLevel, labelLevel)">{{ label }}<span v-if="optional" :class="ccLabel.optional">{{ optionalHelperText }}</span></component>
+    <component :is="labelType" v-if="label" :class="ccLabel.label" :id="labelId" :for="labelFor" :role="valueOrUndefined(labelLevel, 'heading')" :aria-level="valueOrUndefined(labelLevel, labelLevel)">{{ label }}<span v-if="optional" :class="ccLabel.optional">{{ optionalHelperText }}</span></component>
     <slot :triggerValidation="triggerValidation" :labelFor="id" :labelId="labelId" :aria="aria" :hasValidationErrors="isInvalid" />
     <slot name="control" :form="collector" />
-    <div :class="{[ccHelpText.helpText]: true, [ccHelpText.helpTextInvalid]: isInvalid}" v-if="hint || isInvalid">
+    <div :class="{[ccHelpText.helpText]: true, [ccHelpText.helpTextColor]: !isInvalid, [ccHelpText.helpTextColorInvalid]: isInvalid}" v-if="hint || isInvalid">
       <span :id="hintId" v-if="hint" v-html="hint" />
       <span v-if="hint && isInvalid">, </span>
       <span :id="errorId" v-if="isInvalid">{{ errorMessage }}</span>
