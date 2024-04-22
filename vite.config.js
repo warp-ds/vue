42c72d5bb15319c32c9661ca2ef581311d655bd4
Prash Mehra <imprashast@gmail.com>
2024-04-22 15:43:08 +0200

    feat: Individual imports (#162)
    * init
    
    * build individual components
    
    * remove dist from eslint

--- vite.config.js
+++ vite.config.js
@@ -40,8 +40,40 @@ function getBuildOpts(env) {
   if (env.mode === 'lib') {
     return defineConfig({
       build: {
-        ...getLibOpts('warp-vue'),
-        rollupOptions: { external: ['vue'] },
+        lib: {
+          entry: {
+            tag: 'components/tag/index.js',
+            tabs: 'components/tabs/index.js',
+            switch: 'components/switch/index.js',
+            steps: 'components/steps/index.js',
+            slider: 'components/slider/index.js',
+            pill: 'components/pill/index.js',
+            modal: 'components/modal/index.js',
+            generic: 'components/generic/index.js',
+            forms: 'components/forms/index.js',
+            expandable: 'components/expandable/index.js',
+            card: 'components/card/index.js',
+            'button-group': 'components/button-group/index.js',
+            button: 'components/button/index.js',
+            breadcrumbs: 'components/breadcrumbs/index.js',
+            box: 'components/box/index.js',
+            badge: 'components/badge/index.js',
+            attention: 'components/attention/index.js',
+            alert: 'components/alert/index.js',
+            'warp-vue': './index.js',
+          },
+          formats: ['es'],
+        },
+        rollupOptions: {
+          external: ['vue'],
+          output: {
+            format: 'es',
+            dir: 'dist/',
+            entryFileNames: '[name].js',
+            assetFileNames: 'assets/[name].[ext]',
+            chunkFileNames: '[name].js',
+          },
+        },
       },
     });
   }
