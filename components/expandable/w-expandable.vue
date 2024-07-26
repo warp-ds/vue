8fc8a77fb90ce90a4c896a79ffe7f775a401814b
felicia-haggqvist <felicia.haggqvist@schibsted.com>
2024-07-26 16:39:23 +0200

    fix(expandable): add props.info to contentClasses

--- components/expandable/w-expandable.vue
+++ components/expandable/w-expandable.vue
@@ -53,7 +53,11 @@ const chevronUpClasses = computed(() => [ccExpandable.chevronTransform, !expande
 
 const chevronDownClasses = computed(() => [ccExpandable.chevronTransform, expanded.value && ccExpandable.chevronExpand]);
 
-const contentClasses = computed(() => [props.contentClass, props.box && ccBox.box, props.box && hasTitle.value && ccExpandable.paddingTop]);
+const contentClasses = computed(() => [
+  props.contentClass,
+  props.box && ccBox.box,
+  (props.box || props.info) && hasTitle.value && ccExpandable.paddingTop,
+]);
 </script>
 
 <script>
